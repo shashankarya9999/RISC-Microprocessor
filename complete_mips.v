c4e8d44cb43e3c2a047f015a7fcb04d27b05b668
Shashank Arya <shashankarya9831@gmail.com>
2025-02-15 20:07:08 +0530

    Update complete_mips.v

--- complete_mips.v
+++ complete_mips.v
@@ -1,13 +1,17 @@
 // integrating the processor and memory modules
 module Complete_MIPS(CLK, RST, A_Out, D_Out);
+    
     input CLK;
     input RST;
     output [31:0] A_Out;
     output [31:0] D_Out;
     wire CS, WE;
     wire [31:0] ADDR, Mem_Bus;
+    
     assign A_Out = ADDR;
     assign D_Out = Mem_Bus;
+    
     MIPS CPU(CLK, RST, CS, WE, ADDR, Mem_Bus);
     Memory MEM(CS, WE, CLK, ADDR, Mem_Bus);
+    
 endmodule
